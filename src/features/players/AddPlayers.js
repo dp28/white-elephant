57d9c87c851382eaa25aada1573278a236d5b612
Daniel Patterson <dan24patt@gmail.com>
2020-12-03 20:21:45 -0500

    Add a button to copy the invite link

---
+++ src/features/players/AddPlayers.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Card, CardActions, Button, Snackbar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Alert from "@material-ui/lab/Alert";
+
+const useStyles = makeStyles((theme) => ({
+  addPlayers: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+    position: "relative",
+  },
+  message: {
+    position: "absolute",
+    top: "130%",
+    width: "100%",
+  },
+}));
+
+export function AddPlayers() {
+  const classes = useStyles();
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  const hideMessage = () => setShowSuccess(false);
+
+  const copyUrlToClipboard = async () => {
+    const url = window.location.href;
+    await navigator.clipboard.writeText(url);
+    setShowSuccess(true);
+    setTimeout(hideMessage, 2000);
+  };
+
+  return (
+    <div className={classes.addPlayers}>
+      <Card>
+        <CardActions>
+          <Button color="primary" onClick={copyUrlToClipboard}>
+            Copy invite link
+          </Button>
+        </CardActions>
+      </Card>
+      <Snackbar
+        className={classes.message}
+        open={showSuccess}
+        onClose={hideMessage}
+      >
+        <Alert onClose={hideMessage} severity="success">
+          Invite link copied!
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
--- src/features/players/Players.js
+++ src/features/players/Players.js
@@ -4,6 +4,8 @@ import { Paper, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { selectPlayers } from "./playersSlice";
 import { Player } from "./Player";
+import { AddPlayers } from "./AddPlayers";
+import { selectGame, GameStates } from "../game/gameSlice";
 
 const useStyles = makeStyles((theme) => ({
   players: {
@@ -15,10 +17,12 @@ const useStyles = makeStyles((theme) => ({
 export function Players() {
   const classes = useStyles();
   const players = useSelector(selectPlayers);
+  const game = useSelector(selectGame);
 
   return (
     <Paper elevation={0} className={classes.players}>
       <Typography variant="h6">Players</Typography>
+      {game.state === GameStates.WAITING && <AddPlayers />}
       {players.map((player) => (
         <Player player={player} key={player.id} />
       ))}
